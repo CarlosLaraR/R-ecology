6d0b0146e5d1c3f33603be26154654f93ce6c978
Carlos Lara Romero <carlos.lara.romero@gmail.com>
2019-10-24 15:52:57 +0200

    Update filter_decimalsXY.R

--- Data Wrangling/R/filter_decimalsXY.R
+++ Data Wrangling/R/filter_decimalsXY.R
@@ -1,9 +1,11 @@
-#A function to stimate how many decimal places have each case lat/long coordiante and to remove cases without decimal places
+#This function stimate how many decimal places have each case lat/long coordiante and remove imprecise locations without decimal places
+#DF is a data.frame containing at least longitude and latitude 
+#x is a vector with longitude and y is a vector with latitud. 
+#Threshold is the number of decimal places uses to filter the data
 
-filter_decimals = function(x,y,DF) { #x is a vector with longitude, y is a vector with latitud, DF is a data.frame
+filter_decimals <- function(x,y,DF) { 
   require(magrittr)
   require(dplyr)
- 
   #Longitude
   x_nchr = x %>% abs() %>% as.character() %>% nchar() %>% as.numeric()
   x_int = floor(x) %>% abs() %>% nchar()
@@ -17,5 +19,6 @@ filter_decimals = function(x,y,DF) { #x is a vector with longitude, y is a vecto
   y_nchr[y_nchr < 0] = 0
   DF$y_nchr <- y_nchr
   DF<-filter(DF, y_nchr!=0 & x_nchr!=0)
+  DF<-DF[,-c("y_nchr","x_nchr"]
   return(DF)
 } 
