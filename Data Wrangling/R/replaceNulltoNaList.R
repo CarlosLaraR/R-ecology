2b6e014434d309b68b7ed00b52d6bc8b7fa46098
Carlos Lara Romero <carlos.lara.romero@gmail.com>
2020-01-24 16:11:29 +0100

    Update replaceNulltoNaList.R

--- Data Wrangling/R/replaceNulltoNaList.R
+++ Data Wrangling/R/replaceNulltoNaList.R
@@ -1,4 +1,15 @@
-#Replace Null values contained in a list with NA values
+#' replaceNulltoNaList
+#'
+#' Replace Null values contained in a list with NA values
+#'
+#' @param my.list is a list
+#' @param FUN is a function to be parsed (See example)
+#'
+#' @return A data.frame containing the records considered correct.
+#' @export
+#' @examples 
+#' list.NA<- list("a" = 2.5, "b" = NA, "c" = 1:3)
+#' List.new<-replaceNulltoNaList(list.NA , function(x)if(is.null(x))NA else x)
 
 replaceNulltoNaList <- function (my.list, FUN, ...) 
   {
